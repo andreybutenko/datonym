1e08811c3e737dce2c21369431c7e2fc400b00b3
Andrey Butenko <ghostmancer@gmail.com>
2018-08-05 12:01:19 -0700

    Add MVP for finding names with charactaristics

--- analysis/exploration.R
+++ analysis/exploration.R
@@ -46,3 +46,78 @@ FixChronological <- function(df, min.year = min(df$year), max.year = max(df$year
   
   arrange(df, year)
 }
+
+#' Get distribution function for a column in a dataframe such that percentiles
+#' can be calculated.
+#' 
+#' @param df The dataframe
+#' @param col.name Name of the column
+#' @examples
+#' GetLengthPercentile <- GetDistFunction(names.analy, 'length')
+#' GetLengthPercentile(2) # 0.002009762
+#' GetLengthPercentile(6) # 0.5350412
+#' GetLengthPercentile(9) # 0.9730951
+GetDistFunction <- function(df, col.name) {
+  ecdf(df[[col.name]])
+}
+
+#' Pre-calculate distribution functions for numeric columns in a dataframe.
+#' 
+#' @param df The dataframe
+GetCachedDistFunctions <- function(df) {
+  is.numeric.cols <- lapply(df, is.numeric)
+  numeric.cols <- names(numeric.cols)[unlist(numeric.cols)]
+  
+  result <- lapply(numeric.cols, GetDistFunction, df = df)
+  names(result) <- numeric.cols
+  
+  result
+}
+
+dist.functions <- GetCachedDistFunctions(names.analy)
+
+GetPercentileScores <- function(v) {
+  ecdf(v)(v)
+}
+
+FindNames <- function(vowel.factor = 0,
+                      syllable.factor = 0,
+                      symmetry.factor = 0,
+                      masculine.factor = 0,
+                      trendy.factor = 0,
+                      common.factor = 0,
+                      classic.factor = 0,
+                      vowel.ending.factor = 0,
+                      double.letters.factor = 0) {
+  names.analy %>%
+    mutate(score =
+            (vowel.factor * GetPercentileScores(vowels / length)) +
+            (syllable.factor * dist.functions$syllables(syllables)) +
+            (symmetry.factor * dist.functions$symmetry(symmetry)) +
+            (trendy.factor * dist.functions$trendiness.score(trendiness.score)) +
+            (classic.factor * dist.functions$classic.score(classic.score))) %>% 
+    arrange(-score)
+}
+
+# Which name do you find more attractive?
+# or
+# Find names with... (hover for more info)
+#  more vowels, less vowels, no preference
+#  more syllables, less syllables, no preference
+#  more symmetry, less symmetry, no preference
+#  more masculine, more feminine, no preference
+#  more common, less common, no preference
+#  more trendy, less trendy, no preference
+#  more classic, less classic, no preference
+#  more vowel endings, less vowel endings, no preference
+#  more double-letters, less double-letters, no preference
+#  randomize all
+
+# vowel endings
+# Ending in (ette)
+# Double-lettes (nicollette, alessandra)
+# UrbanDictionary?
+names.analy %>% 
+  arrange(-symmetry) %>% 
+  filter(count > 10) %>% 
+  View
