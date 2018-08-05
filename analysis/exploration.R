50aac86f8563ea49c1e5596824302efec3394d0c
Andrey Butenko <ghostmancer@gmail.com>
2018-08-05 16:20:59 -0700

    Add /name endpoint which gets stats and trends on name

--- analysis/exploration.R
+++ analysis/exploration.R
@@ -30,6 +30,36 @@ GetNameTrend <- function(name_) {
     arrange(year)
 }
 
+##' For a given name, returns a list of statistics on that name. If both males
+##' and females have that name, keys with conflicting data will be prepended
+##' with 'm.' for the male value and 'f.' for the female value. Otherwise,
+##' keys will just be the column name with nothing prepended.
+##' 
+##' @param name_ Name to get statistics on.
+GetNameStats <- function(name_) {
+  df <- names.analy %>% 
+    filter(name == name_)
+  
+  if(nrow(df) == 1) {
+    return(purrr::transpose(df)[[1]])
+  }
+  
+  m.df <- filter(df, gender == 'M')
+  f.df <- filter(df, gender == 'F')
+  
+  col.names <- colnames(m.df)
+  
+  for(col.name in col.names) {
+    if(pull(m.df, col.name) != pull(f.df, col.name)) {
+      m.df[[paste0('m.', col.name)]] <- pull(m.df, col.name)
+      m.df[[paste0('f.', col.name)]] <- pull(f.df, col.name)
+      select(m.df, -one_of(col.name))
+    }
+  }
+  
+  return(purrr::transpose(m.df)[[1]])
+}
+
 # Given a dataframe with columns year and count, returns a dataframe where year
 # column contains all years in range (min.year, max.year) with missing values
 # in count filled in with NA.
--- api/plumber.R
+++ api/plumber.R
@@ -10,6 +10,42 @@ EmitError <- function(res, status, msg) {
     return()
 }
 
+#' Return data on name.
+#' 
+#' 
+#' @param name The name
+#' @get /name/<name_>
+function(res, name_) {
+  name <- name_
+  
+  if(missing(name)) {
+    return(EmitError(res, 400, 'Requests to /trend must include ?name parameter'))
+  }
+  
+  if(!NameExists(name)) {
+    return(EmitError(res, 404, paste0('No entries with name "', name, '" found')))
+  }
+  
+  trend.df <- GetNameTrend(name)
+  
+  list(
+    stats = GetNameStats(name),
+    
+    trends = list(
+      male = trend.df %>% 
+        filter(gender == 'M') %>% 
+        FixChronological(min.year = min(trend.df$year),
+                         max.year = max(trend.df$year)),
+      
+      female = trend.df %>% 
+        filter(gender == 'F') %>% 
+        FixChronological(min.year = min(trend.df$year),
+                         max.year = max(trend.df$year))
+    )
+  )
+  
+}
+
 #' Return information on popularity of name over time.
 #' @param name The name
 #' @get /trend
